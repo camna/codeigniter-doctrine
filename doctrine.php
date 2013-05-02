f9a18dbfe8c693e57c0515abb6b22434d8ca7aaf
David Barratt <dbarratt@camna.com>
2013-05-02 17:04:06 -0400

    Initial commit

---
+++ doctrine.php
@@ -0,0 +1,62 @@
+<?php
+
+use Doctrine\Common\ClassLoader;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\DBAL\Logging\EchoSQLLogger;
+
+class Doctrine {
+
+  public $em = null;
+
+  public function __construct()
+  {
+  
+    // load database configuration from CodeIgniter
+    require_once APPPATH.'config/database.php';
+
+    // Set up class loading.
+    require_once FCPATH.'vendor/autoload.php';
+
+    $doctrineClassLoader = new ClassLoader('Doctrine',  APPPATH.'libraries');
+    $doctrineClassLoader->register();
+    $entitiesClassLoader = new ClassLoader('models', rtrim(APPPATH, "/" ));
+    $entitiesClassLoader->register();
+    $proxiesClassLoader = new ClassLoader('Proxies', APPPATH.'models/proxies');
+    $proxiesClassLoader->register();
+
+    // Set up caches
+    $config = new Configuration;
+    $cache = new ArrayCache;
+    $config->setMetadataCacheImpl($cache);
+    $driverImpl = $config->newDefaultAnnotationDriver(array(APPPATH.'models/entities'));
+    $config->setMetadataDriverImpl($driverImpl);
+    $config->setQueryCacheImpl($cache);
+
+    $config->setQueryCacheImpl($cache);
+
+    // Proxy configuration
+    $config->setProxyDir(APPPATH.'/models/proxies');
+    $config->setProxyNamespace('Proxies');
+
+    // Set up logger
+    // $logger = new EchoSQLLogger;
+    // $config->setSQLLogger($logger);
+    
+    $config->setAutoGenerateProxyClasses( TRUE );
+
+    // Database connection information
+    $connectionOptions = array(
+        'driver' => 'pdo_mysql',
+        'user' =>     $db['default']['username'],
+        'password' => $db['default']['password'],
+        'host' =>     $db['default']['hostname'],
+        'dbname' =>   $db['default']['database']
+    );
+
+    // Create EntityManager
+    $this->em = EntityManager::create($connectionOptions, $config);
+    
+  }
+}
