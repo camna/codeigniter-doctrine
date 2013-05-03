6bb4424f1a7b0860c92272d460d51f5f3864fa37
David Barratt <dbarratt@camna.com>
2013-05-03 13:49:11 -0400

    Add the Doctrine_entity class

---
+++ doctrine_entity.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once BASEPATH.'core/model.php';
+
+use Doctrine\Common\ClassLoader;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\DBAL\Logging\EchoSQLLogger;
+
+class Doctrine_entity extends CI_Model {
+
+    public $repository;
+        
+    function __construct() {
+      
+      $this->load->library('doctrine');
+      
+      if (get_called_class() != __CLASS__)
+      {
+        $this->repository = $this->doctrine->em->getRepository(get_called_class());
+      }
+    
+      parent::__construct();
+      
+    }
+    
+}
